f0588cadbe392e3327861f868299c4c59d1a145b
gumi <suzhou.gumi@gmail.com>
2021-09-12 16:06:11 +0800

    feat: HTML解析

---
+++ toy-browser/http/parser.js
@@ -0,0 +1,185 @@
+let currentToken = null;
+
+function emit(token) {
+  console.log(token);
+}
+
+const EOF = Symbol("EOF"); // EOF: End Of File
+
+function data(c) {
+  if (c == "<") {
+    return tagOpen;
+  } else if (c == EOF) {
+    emit({
+      type: "EOF",
+    });
+  } else {
+    emit({
+      type: "text",
+      content: c,
+    });
+    return data;
+  }
+}
+
+function tagOpen(c) {
+  if (c == "/") {
+    return endTagOpen;
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: "startTag",
+      tagName: "",
+    };
+    return tagName(c);
+  } else {
+    return;
+  }
+}
+
+function endTagOpen(c) {
+  if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: "endTag",
+      tagName: "",
+    };
+    return tagName(c);
+  } else if (c == ">") {
+  } else if (c == EOF) {
+  } else {
+  }
+}
+
+function tagName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName;
+  } else if (c == "/") {
+    return selfClosingStartTag;
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    currentToken.tagName += c; // .toLowerCase();
+    return tagName;
+  } else if ((c = ">")) {
+    emit(currentToken);
+    return data;
+  } else {
+    return tagName;
+  }
+}
+
+function beforeAttributeName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName;
+  } else if (c == "/" || c == ">" || c == EOF) {
+    return data;
+  } else if (c == "=") {
+    return beforeAttributeName;
+  } else {
+    currentToken = {
+      name: "",
+      value: "",
+    };
+    return attributeName(c);
+  }
+}
+
+function attributeName(c) {
+  if (c.match(/^[\t\n\f ]$/) || c == "/" || c == ">" || c == EOF) {
+    return afterAttributeName(c);
+  } else if (c == "=") {
+    return beforeAttributeValue;
+  } else if (c == "\u0000") {
+  } else if (c == "'" || c == "" || c == "<") {
+  } else {
+    currentToken.name += c;
+    return attributeName;
+  }
+}
+
+function beforeAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/) || c == "/" || c == ">" || c == EOF) {
+    return beforeAttributeValue;
+  } else if (c == '"') {
+    return doubleQuotedAttributeValue;
+  } else if (c == "'") {
+    return singleQuotedAttributeValue;
+  } else if (c == ">") {
+  } else {
+    return UnquotedAttributeValue(c);
+  }
+}
+
+function doubleQuotedAttributeValue(c) {
+  if (c == '"') {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return afterAttributeValue;
+  } else if (c == "\u0000") {
+  } else if (c == EOF) {
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
+}
+
+function singleQuotedAttributeValue(c) {
+  if (c == "'") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return afterQuotedAttributeValue;
+  } else if (c == "\u0000") {
+  } else if (c == EOF) {
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
+}
+
+function afterQuotedAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName;
+  } else if (c == "/") {
+    return selfClosingStartTag;
+  } else if (c == ">") {
+    currentToken[currentAttribute].name = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c == EOF) {
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
+}
+
+function UnquotedAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return beforeAttributeName;
+  } else if (c == "/") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return selfClosingStartTag;
+  } else if ((c = ">")) {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c == "\u0000") {
+  } else if (c == "'" || c == "'" || c == "<" || c == "=") {
+  } else if (c == EOF) {
+  } else {
+    currentAttribute.value += c;
+    return UnquotedAttributeValue;
+  }
+}
+
+function selfClosingStartTag(c) {
+  if (c == ">") {
+    currentToken.isSelfClosing = true;
+    return data;
+  } else if (c == "EOF") {
+  } else {
+  }
+}
+
+module.exports.parserHTML = function parserHTML(html) {
+  let state = data;
+  for (const c of html) {
+    state = state(c);
+  }
+  state = state(EOF);
+};
